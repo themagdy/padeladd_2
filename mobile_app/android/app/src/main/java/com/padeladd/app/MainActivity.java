d2a900bbd3705704704ccb7ebfe2b267a41ce062
Ahmed Magdy <gitlab@ahmedmagdy.com>
2026-05-03 06:04:51 +0300

    Build v=101: Reverted Java overrides for build stability and switched to Node 22 LTS

--- mobile_app/android/app/src/main/java/com/padeladd/app/MainActivity.java
+++ mobile_app/android/app/src/main/java/com/padeladd/app/MainActivity.java
@@ -1,19 +1,5 @@
 package com.padeladd.app;
 
-import android.os.Bundle;
-import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (getBridge() != null && getBridge().getWebView() != null) {
-            WebView webView = getBridge().getWebView();
-            webView.setVerticalScrollBarEnabled(false);
-            webView.setHorizontalScrollBarEnabled(false);
-            webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-            webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
