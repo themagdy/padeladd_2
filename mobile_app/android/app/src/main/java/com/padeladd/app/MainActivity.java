b3ece10cf2a527a69a53a323ac767e7b2cb0c0f5
Ahmed Magdy <gitlab@ahmedmagdy.com>
2026-05-03 06:11:18 +0300

    Build v=103: The Definitive Java Scrollbar Suppression (100% Fix)

--- mobile_app/android/app/src/main/java/com/padeladd/app/MainActivity.java
+++ mobile_app/android/app/src/main/java/com/padeladd/app/MainActivity.java
@@ -1,5 +1,20 @@
 package com.padeladd.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.view.View;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            WebView webView = getBridge().getWebView();
+            webView.setVerticalScrollBarEnabled(false);
+            webView.setHorizontalScrollBarEnabled(false);
+            webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+            webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        }
+    }
+}
