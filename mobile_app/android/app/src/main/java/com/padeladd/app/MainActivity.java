6309d1e169c8415645a79c8d219f36b4c5cebfe7
Ahmed Magdy <gitlab@ahmedmagdy.com>
2026-05-03 05:59:34 +0300

    Build v=99: Triple-layered native scrollbar suppression (XML, Java, Style)

--- mobile_app/android/app/src/main/java/com/padeladd/app/MainActivity.java
+++ mobile_app/android/app/src/main/java/com/padeladd/app/MainActivity.java
@@ -1,5 +1,19 @@
 package com.padeladd.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            WebView webView = getBridge().getWebView();
+            webView.setVerticalScrollBarEnabled(false);
+            webView.setHorizontalScrollBarEnabled(false);
+            webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+            webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        }
+    }
+}
