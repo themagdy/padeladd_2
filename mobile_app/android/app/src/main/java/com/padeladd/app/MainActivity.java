da3edfa22d49bcb0f84a6f6f73064c392ad3bbb4
Ahmed Magdy <gitlab@ahmedmagdy.com>
2026-05-03 06:14:01 +0300

    Build v=104: Stable build with Extreme XML scrollbar suppression

--- mobile_app/android/app/src/main/java/com/padeladd/app/MainActivity.java
+++ mobile_app/android/app/src/main/java/com/padeladd/app/MainActivity.java
@@ -1,20 +1,5 @@
 package com.padeladd.app;
 
-import android.os.Bundle;
-import android.webkit.WebView;
-import android.view.View;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (getBridge() != null && getBridge().getWebView() != null) {
-            WebView webView = getBridge().getWebView();
-            webView.setVerticalScrollBarEnabled(false);
-            webView.setHorizontalScrollBarEnabled(false);
-            webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
-            webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
